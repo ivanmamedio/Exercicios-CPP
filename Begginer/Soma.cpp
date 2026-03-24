22c624b6d00e7b7fbf5337f74eeef2ccfe686c06
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-23 22:36:07 -0300

    Atualizando

---
+++ Begginer/Soma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::count << "Digite o primeiro número: ";
+    std::cin >> a;
+    std::count << "Digite o segundo número: ";
+    std::cin >> b;
+    std::count << "Soma"
+}
