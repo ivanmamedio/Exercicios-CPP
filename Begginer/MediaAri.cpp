b4237d465b59562451f25416a55cdc1617b22d4c
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-24 22:50:48 -0300

    Atualizando

--- Begginer/MediaAri.cpp
+++ Begginer/MediaAri.cpp
@@ -1,1 +1,9 @@
-#include
+#include <iostream>
+
+int main()
+{
+    float n1, n2, n3;
+    std::cout << "Digite as três notas: ";
+    std::cin >> n1 >> n2 >> n3;
+    float media = (n1 + n2 + n3) / 3;
+}
